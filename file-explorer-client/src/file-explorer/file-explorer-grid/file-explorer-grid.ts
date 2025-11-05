c70ccdd12e8a2f5ff6a1d3d86cc8b3fe3311d9e7
Arno Bornman <bornmana101@gmail.com>
2025-11-05 10:00:55 +0200

    feat: add basic grid component template & display

--- file-explorer-client/src/file-explorer/file-explorer-grid/file-explorer-grid.ts
+++ file-explorer-client/src/file-explorer/file-explorer-grid/file-explorer-grid.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { DirectoryItem } from '../interfaces/directory-item.interface';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'file-explorer-grid',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './file-explorer-grid.html',
   styleUrl: './file-explorer-grid.scss',
 })
 export class FileExplorerGrid {
+  public items = input<DirectoryItem[]>([]);
 
+  public itemClicked = output<DirectoryItem>();
+
+  updateDirectory(data: DirectoryItem) {
+    this.itemClicked.emit(data);
+  }
 }
